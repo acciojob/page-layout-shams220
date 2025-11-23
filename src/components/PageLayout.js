ad7510a93b417a4484973d720b1ae13538a84044
shams alam <103259015+shams220@users.noreply.github.com>
2025-11-23 21:01:55 +0530

    Add PageLayout component with basic structure

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,1 +1,14 @@
-
+import React from "react";
+export default PageLayout = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcom to my website</h1>
+      </header>
+      <p>This is the content of my website</p>
+      <footer>
+        <p>2023 My Website. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
