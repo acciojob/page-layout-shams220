17428598e643107ce7144d55bde3fab979ebb3df
shams alam <103259015+shams220@users.noreply.github.com>
2025-11-23 21:08:15 +0530

    Refactor PageLayout to use props for content

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,14 +1,12 @@
 import React from "react";
-export default PageLayout = () => {
+function PageLayout({ header, children, footer }) {
   return (
     <div>
-      <header>
-        <h1>Welcom to my website</h1>
-      </header>
-      <p>This is the content of my website</p>
-      <footer>
-        <p>© 2023 My Website. All rights reserved.</p>
-      </footer>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>{footer}</footer>
     </div>
   );
-};
+}
+
+export default PageLayout;
