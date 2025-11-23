d1f8632ef5af26c77604da7c7bdd9920be8a4026
shams alam <103259015+shams220@users.noreply.github.com>
2025-11-23 21:07:30 +0530

    Update PageLayout component with header and footer

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,12 @@ import PageLayout from './PageLayout.js';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    <PageLayout/>
+    <PageLayout
+      header={<h1>Welcome to my website</h1>}
+      footer={<p>© 2023 My Website. All rights reserved.</p>}
+    >
+      <p>This is the content of my website.</p>
+    </PageLayout>
     </div>
   )
 }
